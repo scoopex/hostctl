ada02415eb58416de5abc2d9cb82f89cfaf8140c
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-04-23 15:26:26 +0200

    add output

--- src/execute.rs
+++ src/execute.rs
@@ -1,10 +1,11 @@
 use crate::utils::output;
 use crate::utils::OutputType;
 
-pub fn execute_node(node: String, iter_information: String, local_execution: bool, execution_file: &String) {
+pub fn execute_node(node: String, iter_information: String, local_execution: bool, execution_string: &String) {
     let mut output_text: String = format!("*** HOST: {node} {iter_information}");
     if local_execution {
         output_text = format!("*** LOCAL: {node} {iter_information}");
     }
     output(output_text, OutputType::Info);
+    output(format!(">>>{execution_string}<<<"), OutputType::Debug);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -13,6 +13,8 @@ pub enum OutputType {
 pub fn output(msg: String, level: OutputType){
     if level == OutputType::Info {
         println!("{color_green}{msg}{color_reset}")
+    }else if level == OutputType::Debug {
+        println!("{color_yellow}{msg}{color_reset}")
     }else{
         println!("{color_red}{msg}{color_reset}")
     }
