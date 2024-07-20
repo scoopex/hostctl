8f2a6ee934fc2b99ec950ce055d7f136d3e22bbf
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-07-20 17:51:32 +0200

    show execution status
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- src/execute.rs
+++ src/execute.rs
@@ -33,6 +33,41 @@ fn execute_local(templated_lines: Vec<String>) -> bool {
     return true;
 }
 
+fn execute_remote(node: String, templated_lines: Vec<String>) -> bool {
+
+    for (nr, line) in templated_lines.iter().enumerate() {
+        output(format!("#{nr}: {line}"), OutputType::Debug);
+    }
+
+    let mut cmd = Command::new("ssh");
+    cmd.arg(node);
+    cmd.arg("bash");
+    cmd.arg("-s");
+
+    //cmd.arg(temp_file_path.unwrap().to_string());
+
+    cmd.stdin(Stdio::piped())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
+
+    let mut child = cmd.spawn().expect("Unable to start remote connection");
+
+    if let Some(mut stdin) = child.stdin.take() {
+        for line in templated_lines.iter() {
+            write!(stdin, "{}", line).expect("Failed to write to stdin");
+        }
+    } else {
+        eprintln!("Failed to open stdin");
+    }
+
+    let status = child.wait().expect("Failed to wait for script");
+    if !status.success() {
+        output(format!("FAILED, EXITCODE WAS : {}", status.code().unwrap()),OutputType::Error);
+        return false;
+    }
+    output("\nSUCCESS".to_string(), OutputType::Info);
+    return true;
+}
 
 pub fn execute_node(node: String, iter_information: String, local_execution: bool, execution_lines: &Vec<String>) -> bool {
     let mut output_text: String = format!("*** HOST: {node} {iter_information}\n");
@@ -45,8 +80,7 @@ pub fn execute_node(node: String, iter_information: String, local_execution: boo
     if local_execution {
         return execute_local(templated_lines);
     }else{
-        output("NOT IMPLEMENTED".to_string(), OutputType::Error);
-        return false;
+        return execute_remote(node, templated_lines);
     }
 }
 
@@ -61,7 +95,7 @@ fn template_lines(execution_lines: &Vec<String>, node: &String) -> Vec<String> {
 pub fn execute_nodes(nodes: Vec<String>, only_nodes: bool, execute_local: bool, execution_lines: &Vec<String>) -> i32 {
     let number_of_nodes = nodes.len();
     let mut number_of_current = 0;
-    let mut failed_host = 0;
+    let mut failed_nodes: Vec<String> = Vec::new();
     for node in nodes {
         number_of_current += 1;
         let iter_info: String;
@@ -71,9 +105,15 @@ pub fn execute_nodes(nodes: Vec<String>, only_nodes: bool, execute_local: bool, 
             let membership_info = "Nodes";
             iter_info = format!("({membership_info} [{number_of_current}/{number_of_nodes}])");
         }
-        if execute_node(node, iter_info.to_string(), execute_local, &execution_lines){
-            failed_host += 1;
+        if !execute_node(node.clone(), iter_info.to_string(), execute_local, &execution_lines){
+            failed_nodes.push(node)
         }
     }
-    return failed_host;
+    if failed_nodes.len() > 0 {
+        let failed_nodes_str = failed_nodes.join(", ");
+        output(format!("\n\nCOMPLETED  - ONE OR MORE NODES FAILED!\n\nFAILED NODES: {failed_nodes_str}"), OutputType::Error);
+    }else{
+        output(format!("\n\nCOMPLETED - ALL NODES WERE SUCCESSFUL"), OutputType::Info);
+    }
+    failed_nodes.len() as i32
 }
